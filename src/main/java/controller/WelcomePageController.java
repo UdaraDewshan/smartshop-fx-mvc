1c438650387cfc3fb79a4cb7b01412c994b503d2
UdaraDewshan <dewudara@gmail.com>
2025-11-16 22:21:55 +0530

    Add Welcome Page Controller

--- src/main/java/controller/WelcomePageController.java
+++ src/main/java/controller/WelcomePageController.java
@@ -1,8 +1,9 @@
 package controller;
 
-import javafx.event.ActionEvent;
+import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.util.Duration;
 
 public class WelcomePageController {
 
@@ -10,8 +11,38 @@ public class WelcomePageController {
     private Button btnGo;
 
     @FXML
-    void handleGoAction(ActionEvent event) {
+    public void initialize() {
+        addButtonHoverAnimation();
+    }
+
+    @FXML
+    private void handleGoAction() {
+
+        ScaleTransition st = new ScaleTransition(Duration.millis(150), btnGo);
+        st.setFromX(1.0);
+        st.setFromY(1.0);
+        st.setToX(0.90);
+        st.setToY(0.90);
+        st.setAutoReverse(true);
+        st.setCycleCount(2);
+        st.play();
 
     }
 
+    private void addButtonHoverAnimation() {
+
+        btnGo.setOnMouseEntered(e -> {
+            ScaleTransition st = new ScaleTransition(Duration.millis(150), btnGo);
+            st.setToX(1.12);
+            st.setToY(1.12);
+            st.play();
+        });
+
+        btnGo.setOnMouseExited(e -> {
+            ScaleTransition st = new ScaleTransition(Duration.millis(150), btnGo);
+            st.setToX(1.0);
+            st.setToY(1.0);
+            st.play();
+        });
+    }
 }
