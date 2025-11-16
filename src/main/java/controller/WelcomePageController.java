098946e309cba1691e62b8be2b31e1808a4f2ed0
UdaraDewshan <dewudara@gmail.com>
2025-11-16 16:56:59 +0530

    Add Welcome Page Fxml

---
+++ src/main/java/controller/WelcomePageController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class WelcomePageController {
+
+    @FXML
+    private Button btnGo;
+
+    @FXML
+    void handleGoAction(ActionEvent event) {
+
+    }
+
+}
